39cae9122e1afddbfe4f77362813a24e68cdfcb6
Thamer Al-harbi <33045743+thxa@users.noreply.github.com>
2025-07-02 01:42:00 +0300

    Fix: url

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,8 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
+// export default defineConfig(({ mode }) => ({
+export default defineConfig({
   // server: {
   //   host: "::",
   //   port: 8080,
@@ -15,10 +16,10 @@ export default defineConfig(({ mode }) => ({
   plugins: [
     react(),
   ],
-  base: "/activity-pulse-portfolio",
+  base: "/activity-pulse-portfolio/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-}));
+});
